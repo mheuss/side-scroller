ea88b8728fb584485b3fd701373ac9727efddf5b
Michael Heuss <mrheuss@gmail.com>
2024-05-25 17:05:29 -0400

    Animate opening and closing of crevice

--- src/models/canyon.ts
+++ src/models/canyon.ts
@@ -1,37 +1,91 @@
 import { Sprite } from "src/models/sprite";
 import { P5CanvasInstance } from "@p5-wrapper/react";
 import { colors } from "src/constants";
+import { randomInt } from "src/utilities";
 
 const MAX_WIDTH = 50;
 const MIN_WIDTH = 0;
 
+interface ICrevice {
+  width: number;
+  velocity: number;
+  chanceForChange: number;
+}
+
 export class Canyon extends Sprite {
-  private width; // Width or spread of the chasm
+  private crevice: ICrevice; // Width or spread of the chasm
+
   constructor(
     p5: P5CanvasInstance,
     x: number,
     y: number,
     scale: number,
-    width: number = 0,
+    crevice: number = 0,
   ) {
     super(p5, x, y, scale ?? 1);
 
-    this.width = p5.constrain(width, MIN_WIDTH, MAX_WIDTH);
+    this.crevice = {
+      width: p5.constrain(crevice, MIN_WIDTH, MAX_WIDTH),
+      velocity: 0,
+      chanceForChange: 0,
+    };
   }
+
+  /**
+   * When set to a number be 1 and 100, reflects the chance of the crevice width
+   * changing during the draw cycle.
+   *
+   * The speed of the change is random
+   * @param chance
+   */
+  public setChanceForCreviceWidthChange = (chance: number) => {
+    this.crevice.chanceForChange = chance;
+  };
+
+  /**
+   * Private function that will determine if the crevice width should change.
+   * If it decides it should, it'll randomly change the velocity of the crevice
+   * If the crevice was changing, it could stop.
+   */
+  private shouldCreviceChange = () => {
+    const { crevice } = this;
+
+    const changePercent = randomInt(0, 100);
+
+    if (changePercent < crevice.chanceForChange) {
+      crevice.velocity = randomInt(0, 4) - 2;
+    }
+
+    if (crevice.velocity === 0) {
+      return;
+    }
+
+    crevice.width += crevice.velocity;
+
+    if (crevice.width >= MAX_WIDTH) {
+      crevice.velocity = 0;
+      crevice.width = MAX_WIDTH;
+    } else if (crevice.width <= MIN_WIDTH) {
+      crevice.velocity = 0;
+      crevice.width = MIN_WIDTH;
+    }
+  };
 
   public draw() {
     const { blueSky, grassGreen, darkerGray, midGray, stoneGray } = colors;
-    const { width } = this;
+    const { crevice } = this;
+
+    this.shouldCreviceChange();
 
     this.processArray([
       // First Spread
       ["stroke", blueSky],
       ["strokeWeight", 1],
       ["noFill"],
-      ["line", 0, 0, 158 + width, 0], //draw a horizon line
+      ["line", 0, 0, 158 + crevice.width, 0], //draw a horizon line
       ["noStroke"],
       ["fill", blueSky],
-      ["rect", 2, 0, 158 + width, 200],
+      ["rect", 2, 0, 158 + crevice.width, 200],
       ["fill", darkerGray],
       ["noStroke"],
       ["beginShape"],
@@ -62,44 +116,44 @@ export class Canyon extends Sprite {
       ["vertex", 15, 200],
       ["endShape", this.p5.CLOSE],
       ["rect", 0, 0, 11, 200],
-      ["rect", 158 + width, 0, 5, 200],
+      ["rect", 158 + crevice.width, 0, 5, 200],
       // Second Half
       ["fill", stoneGray],
       [
         "quad",
-        100 + width,
+        100 + crevice.width,
         10,
-        150 + width,
+        150 + crevice.width,
         100,
-        150 + width,
+        150 + crevice.width,
         140,
-        150 + width,
+        150 + crevice.width,
         10,
       ],
       ["fill", midGray],
       [
         "quad",
-        100 + width,
+        100 + crevice.width,
         100,
-        150 + width,
+        150 + crevice.width,
         150,
-        150 + width,
+        150 + crevice.width,
         170,
-        150 + width,
+        150 + crevice.width,
         100,
       ],
       ["fill", grassGreen],
       ["beginShape"],
-      ["vertex", 160 + width, 0],
-      ["curveVertex", 130 + width, 0],
-      ["curveVertex", 100 + width, 10],
-      ["curveVertex", 130 + width, 35],
-      ["curveVertex", 100 + width, 10],
-      ["curveVertex", 130 + width, 35],
-      ["curveVertex", 140 + width, 80],
-      ["curveVertex", 100 + width, 100],
-      ["vertex", 145 + width, 120],
-      ["vertex", 160 + width, 200],
+      ["vertex", 160 + crevice.width, 0],
+      ["curveVertex", 130 + crevice.width, 0],
+      ["curveVertex", 100 + crevice.width, 10],
+      ["curveVertex", 130 + crevice.width, 35],
+      ["curveVertex", 100 + crevice.width, 10],
+      ["curveVertex", 130 + crevice.width, 35],
+      ["curveVertex", 140 + crevice.width, 80],
+      ["curveVertex", 100 + crevice.width, 100],
+      ["vertex", 145 + crevice.width, 120],
+      ["vertex", 160 + crevice.width, 200],
       ["endShape", this.p5.CLOSE],
       ["noStroke"],
       ["stroke", 0],
